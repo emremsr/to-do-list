4b28b74fe8942cdff6892aaa895959316d8024c5
Emre Mısırlıoğlu <emremsr95@gmail.com>
2023-08-18 23:40:03 +0300

    first commit
    My project, which I will upload to GitHub, includes a To Do list application that I developed using Vue 3 and Vuex. Users can add and delete tasks. I carefully styled the design with Tailwind CSS, resulting in a modern and user-friendly interface. This application is designed to help you easily manage and track your tasks. You can find the details on GitHub.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,63 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div class="from-slate-700 to-slate-900 bg-gradient-to-r text-white h-screen">
+    
+    <div class="flex flex-col items-center justify-center pt-36">
+      <h1 class="text-7xl mb-5 bg-gradient-to-r from-slate-500 to-green-500 text-transparent bg-clip-text">To-Do List</h1>
+      <div class="flex gap-5">
+        <input
+          class="w-52 text-black p-2 hover:w-56 rounded-xl outline-4 transition-all duration-1000"
+          v-model="newTodo"
+          placeholder="Add a new task"
+        />
+        <button
+          class="px-4 py-2 bg-green-500 rounded-r-3xl hover:rounded-l-3xl hover:rounded-r-none transition-all duration-1000"
+          @click="addTodo"
+        >
+          Add
+        </button>
+      </div>
+      <div class="pt-8">
+        <ul>
+          <li v-for="(todo, index) in todos" :key="index">
+            <div
+              class="flex items-center justify-between w-72 mb-2 p-2 rounded-t-xl hover:rounded-t-none hover:rounded-b-xl border capitalize transition-all duration-200"
+            >
+              <p>{{ todo }}</p>
+              <button class="px-2 rounded-md  hover:bg-red-700 transition-colors duration-500" @click="removeTodo(index)">Remove</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { mapGetters, mapActions } from "vuex";
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  data() {
+    return {
+      newTodo: "",
+    };
+  },
+  computed: {
+    ...mapGetters(["allTodos"]),
+    todos() {
+      return this.allTodos;
+    },
+  },
+  methods: {
+    ...mapActions(["addTodoAsync"]),
+    addTodo() {
+      if (this.newTodo) {
+        this.addTodoAsync(this.newTodo);
+        this.newTodo = "";
+      }
+    },
+    removeTodo(index) {
+      this.$store.commit("removeTodo", index);
+    },
+  },
+};
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,29 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
 
 export default createStore({
-  state: {
-  },
-  getters: {
+  state() {
+    return {
+      todos: [],
+    };
   },
   mutations: {
+    addTodo(state, todo) {
+      state.todos.push(todo);
+    },
+    removeTodo(state, index) {
+      state.todos.splice(index, 1);
+    },
   },
   actions: {
+    addTodoAsync({ commit }, todo) {
+      setTimeout(() => {
+        commit('addTodo', todo);
+      }, 1000);
+    },
   },
-  modules: {
-  }
-})
+  getters: {
+    allTodos(state) {
+      return state.todos;
+    },
+  },
+});
